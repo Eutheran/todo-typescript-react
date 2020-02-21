0efce3abb774ec198ffc12d5d39ac3582889b16a
Eric Kreiter <ekreiter@Erics-MacBook-Pro.local>
2020-02-21 12:06:54 -0500

    Update: Built a single todo list item

---
+++ src/components/todo-list-item/todo-list-item.component.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./todo-list-item.component.css";
+
+//interfaces gives composition of what to expect
+//types declared from type definition file are brought in and aliased here
+interface TodoListeItemProps {
+  todo: Todo;
+  toggleTodo: ToggleTodo;
+}
+
+//destructuring what is passed in for easy access
+export const ToDoListItem: React.FunctionComponent<TodoListeItemProps> = ({
+  todo,
+  toggleTodo,
+}) => {
+  return (
+    <li>
+      <label className={todo.complete ? "complete" : undefined}>
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={() => toggleTodo(todo)}
+        />
+        {todo.text}
+      </label>
+    </li>
+  );
+};
