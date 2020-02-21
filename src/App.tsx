42a5956a6b35b977dda4007fd26bb094af5a2cbc
Eric Kreiter <ekreiter@luc.id>
2020-02-21 12:14:00 -0500

    feat: staged app and added single todo item

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ToDoListItem } from "./components/todo-list-item/todo-list-item.component";
 
-function App() {
+const initialTodos: Array<Todo> = [
+  { text: "walk the dog", complete: true },
+  { text: "write app", complete: false },
+];
+
+const App: React.FunctionComponent = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  //by specifying toggleTodo we no longer have
+  //to declare what selectedTodo will follow
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if ((todo = selectedTodo)) {
+        return { ...todo, complete: !todo.complete };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {todos.map(todo => {
+        return (
+          <>
+            <ToDoListItem todo={todo} toggleTodo={toggleTodo} />
+          </>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
